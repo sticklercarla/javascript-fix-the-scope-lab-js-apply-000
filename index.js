929c0f232b8b037d848366c2a6b594f6a525b5b6
Carla Stickler <carlastickler@gmail.com>
2019-02-15 01:33:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ function yourAnimal() {
   // P.S.: You can't just hard-code 'cat' below
   return otherAnimal
 }
+const two = 2
 
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
